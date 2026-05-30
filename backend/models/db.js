e161ee1566ae651fa9e4409671993157d046bac6
Sagnik Paul <sagnikpaul@Sagniks-MacBook-Air.local>
2026-05-30 18:07:18 +0530

    Railway database connected

--- backend/models/db.js
+++ backend/models/db.js
@@ -3,13 +3,18 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Sagnik2006',
-  database: process.env.DB_NAME || 'event_booking',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
 module.exports = pool;
